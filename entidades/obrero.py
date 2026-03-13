e07a34c166587abaecac59582a8a2747421d654e
RymonPuffs <rymon344@gmail.com>
2026-03-12 19:42:50 -0700

    se agrego la especialidad técnica del obrero

--- entidades/obrero.py
+++ entidades/obrero.py
@@ -3,5 +3,6 @@ from datetime import datetime
 
 # Clase Obrero que hereda de Nomina
 class Obrero(Nomina):
-    def __init__(self, empleado: str, fecha_emision: datetime, bonos: float, sueldo: float, dias_trabajados: int):
+    def __init__(self, empleado: str, fecha_emision: datetime, bonos: float, sueldo: float, dias_trabajados: int, especialidad_tecnica: str):
         super().__init__(empleado, fecha_emision, bonos, sueldo, dias_trabajados)
+        self.especialidad_tecnica = especialidad_tecnica
