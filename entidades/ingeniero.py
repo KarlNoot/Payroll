84292a7a01213263842300def5cbe702938022b6
RymonPuffs <rymon344@gmail.com>
2026-03-12 19:42:20 -0700

    Se agrega la cedula profecional

--- entidades/ingeniero.py
+++ entidades/ingeniero.py
@@ -3,5 +3,6 @@ from datetime import datetime
 
 # Clase Ingeniero que hereda de Nomina
 class Ingeniero(Nomina):
-    def __init__(self, empleado: str, fecha_emision: datetime, bonos: float, sueldo: float, dias_trabajados: int):
+    def __init__(self, empleado: str, fecha_emision: datetime, bonos: float, sueldo: float, dias_trabajados: int, cedula_profecional: str):
         super().__init__(empleado, fecha_emision, bonos, sueldo, dias_trabajados)
+        self.cedula_profecional = cedula_profecional
